1df7e4c2e00268ddb7a93183b7437fe6669b060e
mobilejon <40245473+mobilejon@users.noreply.github.com>
2020-07-16 11:38:05 -0400

    Update anyconnect.sh

--- anyconnect.sh
+++ anyconnect.sh
@@ -3,6 +3,9 @@
 myHost=$(hostname | /usr/bin/awk -F's-' '{print $1}')
 cert=`security find-certificate -c $myHost -Z | grep SHA | awk '{print $3}'`
 loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
+##Set Safari to use certificate
+security set-identity-preference -Z $cert -s *.vmwareidentity.com
+security set-identity-preference -Z $cert -s cas-aws.vmwareidentity.com
 #Create .anyconnect config file
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <AnyConnectPreferences>
@@ -10,9 +13,9 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <DefaultSecondUser></DefaultSecondUser>
 <ClientCertificateThumbprint>$cert</ClientCertificateThumbprint>
 <ServerCertificateThumbprint></ServerCertificateThumbprint>
-<DefaultHostName>vpn.test.com</DefaultHostName>
-<DefaultHostAddress>vpn.test.com</DefaultHostAddress>
-<DefaultGroup>VPNGROUP</DefaultGroup>
+<DefaultHostName>vpn.mobilejon.com</DefaultHostName>
+<DefaultHostAddress>vpn.mobilejon.com</DefaultHostAddress>
+<DefaultGroup>MOBILEJONVPN</DefaultGroup>
 <ProxyHost></ProxyHost>
 <ProxyPort></ProxyPort>
 <SDITokenType>none</SDITokenType>
